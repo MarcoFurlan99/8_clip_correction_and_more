c785344c3e6fe11bb66b243342abf69601d0be16
MarcoFurlan99 <marcofurlan99@gmail.com>
2023-08-03 16:53:15 +0200

    c

--- code/geom_seq_gen.py
+++ code/geom_seq_gen.py
@@ -1,8 +1,12 @@
-start = 16
+start = 24
 end = 128
-n = 5
+n = 7
 
 step = (end/start)**(1/(n-1))
 
+l = []
+
 for i in range(n):
-    print(round(start*step**i))
+    l.append(round(start*step**i))
+
+print(l)
